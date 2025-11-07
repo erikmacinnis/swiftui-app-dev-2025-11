2b67123af33940b2704be29a3b6df501fd051669
erikmacinnis <erikhyun35@gmail.com>
2025-11-07 12:39:01 -0700

    feat: video player removed default playback controls

--- app/VideoPlayer/View/VideoPlayer/VideoPlayerView.swift
+++ app/VideoPlayer/View/VideoPlayer/VideoPlayerView.swift
@@ -8,12 +8,29 @@
 import SwiftUI
 import AVKit
 
+// Displays the video player without the playback controls
+// Got code from here: https://stackoverflow.com/questions/65927459/playback-controls-in-swiftui
+struct CustomVideoPlayer: UIViewControllerRepresentable {
+    let player: AVPlayer?
+    
+    func makeUIViewController(context: Context) -> AVPlayerViewController {
+        let controller = AVPlayerViewController()
+        controller.showsPlaybackControls = false
+        controller.player = player
+        return controller
+    }
+    
+    func updateUIViewController(_ uiViewController: AVPlayerViewController, context: Context) {
+        uiViewController.player = player
+    }
+}
+
 struct VideoPlayerView: View {
     // handles all app logic and states
     @State private var controller = VideoPlayerController()
     
     var body: some View {
         // Display video
-        VideoPlayer(player: controller.player)
+        CustomVideoPlayer(player: controller.player)
     }
 }
